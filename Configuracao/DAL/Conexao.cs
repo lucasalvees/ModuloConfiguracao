c4b236b72c4ac48ad8f8d41d70d9b2385e1c393d
Lucas Alves <sorriso_colgati2711@hotmail.com>
2023-03-15 16:48:19 -0300

    StringDeConexao

---
+++ Configuracao/DAL/Conexao.cs
@@ -0,0 +1,16 @@
+﻿namespace DAL
+{
+    public static class Conexao
+    {
+        public static string StringDeConexao 
+        { 
+            get
+            {
+                return @"User ID=SA;
+                         Initial Catalog=Configuracao
+                         Data Source=.\SQLEXPRESS2019
+                         Password=Senailab02";
+            }
+        }
+    }
+}
