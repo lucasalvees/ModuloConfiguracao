ba9ce7c042925e56a8c2c69c55474f906df7369b
Lucas Alves <sorriso_colgati2711@hotmail.com>
2023-03-17 17:20:52 -0300

    Criação de GrupoUsuarioBLL

---
+++ Configuracao/BLL/GrupoUsuarioBLL.cs
@@ -0,0 +1,47 @@
+﻿using DAL;
+using Models;
+using System.Collections.Generic;
+using System;
+
+namespace BLL
+{
+    public class GrupoUsuarioBLL
+    {
+        public void Inserir(GrupoUsuario _grupoUsuario)
+        {
+            ValidarDados(_grupoUsuario);
+
+            GrupoUsuarioDAL usuarioDAL = new GrupoUsuarioDAL();
+            usuarioDAL.Inserir(_grupoUsuario);
+
+        }
+        public void Alterar(GrupoUsuario _grupoUsuario)
+        {
+            ValidarDados(_grupoUsuario);
+
+            GrupoUsuarioDAL grupoUsuarioDAL = new GrupoUsuarioDAL();
+            grupoUsuarioDAL.Alterar(_grupoUsuario);
+        }
+        public void Excluir(int _id)
+        {
+            new GrupoUsuarioDAL().Excluir(_id);
+        }
+        public List<GrupoUsuario> BuscarTodos()
+        {
+            return new GrupoUsuarioDAL().BuscarTodos();
+        }
+        public GrupoUsuario BuscarPorId(int _id)
+        {
+            return new GrupoUsuarioDAL().BuscarPorId(_id);
+        }
+        public List<GrupoUsuario> BuscarPorNomeGrupo(string _nomeGrupo)
+        {
+            return new GrupoUsuarioDAL().BuscarPorNomeGrupo(_nomeGrupo);
+        }
+        private void ValidarDados(GrupoUsuario _grupoUsuario)
+        {
+            if (_grupoUsuario.NomeGrupo.Length <= 2)
+                throw new Exception("O nome deve ter mais de 2 caracteres.");
+        }
+    }
+}
