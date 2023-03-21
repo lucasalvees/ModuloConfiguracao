96087768230dcc8a52fc56978aace494f1b02e0e
Lucas Alves <sorriso_colgati2711@hotmail.com>
2023-03-21 17:14:17 -0300

    Criação FormCadastroGrupoUsuario

---
+++ Configuracao/WindowsFormsAppPrincipal/FormCadastroGrupoUsuario.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace WindowsFormsAppPrincipal
+{
+    partial class FormCadastroGrupoUsuario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Label nomeGrupoLabel;
+            this.grupoUsuarioBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.nomeGrupoTextBox = new System.Windows.Forms.TextBox();
+            this.buttonSalvar = new System.Windows.Forms.Button();
+            this.buttonCancelar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            nomeGrupoLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.grupoUsuarioBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // nomeGrupoLabel
+            // 
+            nomeGrupoLabel.AutoSize = true;
+            nomeGrupoLabel.Location = new System.Drawing.Point(22, 99);
+            nomeGrupoLabel.Name = "nomeGrupoLabel";
+            nomeGrupoLabel.Size = new System.Drawing.Size(101, 16);
+            nomeGrupoLabel.TabIndex = 2;
+            nomeGrupoLabel.Text = "Nome do grupo";
+            // 
+            // grupoUsuarioBindingSource
+            // 
+            this.grupoUsuarioBindingSource.DataSource = typeof(Models.GrupoUsuario);
+            // 
+            // nomeGrupoTextBox
+            // 
+            this.nomeGrupoTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.grupoUsuarioBindingSource, "NomeGrupo", true));
+            this.nomeGrupoTextBox.Location = new System.Drawing.Point(129, 96);
+            this.nomeGrupoTextBox.Multiline = true;
+            this.nomeGrupoTextBox.Name = "nomeGrupoTextBox";
+            this.nomeGrupoTextBox.Size = new System.Drawing.Size(615, 25);
+            this.nomeGrupoTextBox.TabIndex = 3;
+            // 
+            // buttonSalvar
+            // 
+            this.buttonSalvar.Location = new System.Drawing.Point(530, 366);
+            this.buttonSalvar.Name = "buttonSalvar";
+            this.buttonSalvar.Size = new System.Drawing.Size(75, 23);
+            this.buttonSalvar.TabIndex = 14;
+            this.buttonSalvar.Text = "Salvar";
+            this.buttonSalvar.UseVisualStyleBackColor = true;
+            this.buttonSalvar.Click += new System.EventHandler(this.buttonSalvar_Click);
+            // 
+            // buttonCancelar
+            // 
+            this.buttonCancelar.Location = new System.Drawing.Point(611, 366);
+            this.buttonCancelar.Name = "buttonCancelar";
+            this.buttonCancelar.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancelar.TabIndex = 14;
+            this.buttonCancelar.Text = "Cancelar";
+            this.buttonCancelar.UseVisualStyleBackColor = true;
+            this.buttonCancelar.Click += new System.EventHandler(this.buttonCancelar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(17, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(370, 46);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Cadastro de grupos";
+            // 
+            // FormCadastroGrupoUsuario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 168);
+            this.Controls.Add(this.buttonCancelar);
+            this.Controls.Add(this.buttonSalvar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(nomeGrupoLabel);
+            this.Controls.Add(this.nomeGrupoTextBox);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormCadastroGrupoUsuario";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Cadastro de grupos";
+            this.Load += new System.EventHandler(this.FormCadastroGrupoUsuario_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.grupoUsuarioBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.BindingSource grupoUsuarioBindingSource;
+        private System.Windows.Forms.TextBox nomeGrupoTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonSalvar;
+        private System.Windows.Forms.Button buttonCancelar;
+    }
+}
---
+++ Configuracao/WindowsFormsAppPrincipal/FormCadastroGrupoUsuario.cs
@@ -0,0 +1,49 @@
+﻿using BLL;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppPrincipal
+{
+    public partial class FormCadastroGrupoUsuario : Form
+    {
+        public int Id;
+        public FormCadastroGrupoUsuario(int _id = 0)
+        {
+            InitializeComponent();
+            Id = _id;
+        }
+
+        private void FormCadastroGrupoUsuario_Load(object sender, EventArgs e)
+        {
+            if (Id == 0)
+                grupoUsuarioBindingSource.AddNew();
+            else
+                grupoUsuarioBindingSource.DataSource = new GrupoUsuarioBLL().BuscarPorId(Id);
+        }
+        private void buttonSalvar_Click(object sender, EventArgs e)
+        {
+            GrupoUsuarioBLL grupoUsuarioBLL = new GrupoUsuarioBLL();
+            grupoUsuarioBindingSource.EndEdit();
+
+            if (Id == 0)
+                grupoUsuarioBLL.Inserir((GrupoUsuario)grupoUsuarioBindingSource.Current);
+            else
+                grupoUsuarioBLL.Alterar((GrupoUsuario)grupoUsuarioBindingSource.Current);
+
+            MessageBox.Show("Registro salvo com sucesso!");
+            Close();
+        }
+        private void buttonCancelar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
